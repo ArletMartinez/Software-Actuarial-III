467ea44d78e0ac20ea190b7d3bc2955980ab496b
Arlet <aralo-98@hotmail.com>
2018-03-05 09:35:12 -0600

    cambio
    No lo habia guardado

--- tarea.R
+++ tarea.R
@@ -1,3 +1,10 @@
+#creamos objeto
 a <- data.frame(mtcars)
+#vemos como se gurda
 dput(a)
-dput(a, file = ".R")
+#lo guardamos en nuevo archivo en el disco
+dput(a, file = "Carros.R")
+#lo extraemos con otro objeto
+a2 <- dget("Carros.R")
+a2
+dput(a2, file = "CarrosArlet.R")
